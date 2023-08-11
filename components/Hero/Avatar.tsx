812196de1a5e961b440bb1f1c28d6f7ba745d4da
AJFuturistic <ajfuturistics@gmail.com>
2023-08-11 20:07:04 +0530

    updated avatar

--- components/Hero/Avatar.tsx
+++ components/Hero/Avatar.tsx
@@ -1,13 +1,23 @@
 "use client";
 
-import Spline from "@splinetool/react-spline";
+import Image from "next/image";
+// import Spline from "@splinetool/react-spline";
 import React from "react";
 
 const Avatar = () => {
   return (
     <div className="w-full h-full flex justify-center items-center">
-      <div className="bg-[#9a7f49] dark:bg-white w-52 h-52 rounded-full">
-        <Spline scene="/spline/scene.splinecode" />
+      {/* <div className="bg-[#9a7f49] dark:bg-white w-52 h-52 rounded-full"> */}
+      <div className="w-80 rounded-full">
+        {/* <Spline scene="/spline/scene.splinecode" /> */}
+        <Image
+          width="0"
+          height="0"
+          sizes="100vw"
+          className="w-full h-full"
+          src="/assets/images/wfh.gif"
+          alt="avatar"
+        />
       </div>
     </div>
   );
